60fd4bf49bc5caf99dc984eae32ee1ddd18ef13a
Andrei Poehlmann <an.poehlmann@gmail.com>
2025-11-02 13:13:13 +0100

    fix versioning, use >= instead to avoid upper bounding

--- modules/vpc/versions.tf
+++ modules/vpc/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "~> 6.0"
+        version = ">= 6.0"
     }
   }
+  required_version = ">= 1.13"
 }
